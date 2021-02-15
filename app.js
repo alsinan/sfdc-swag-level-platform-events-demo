dec71c27de2289ad353ec3cecb8360c656a79305
Alsinan Nazim <anazim@suyatitech.local>
2021-02-15 12:10:35 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -158,9 +158,6 @@ function subscribeToEvents( sfClient, res ) {
 
     // http://paulbattisson.com/blog/2017/consuming-platform-events-in-10-lines-of-javascript/
     sfClient.streaming.topic( '/event/Sample_Event__e' ).subscribe( function( message ) {
-
-        // Send each new message as it arrives
-
         console.log( '-- RECEIVED EVENT -----------------------------------------------' );
         console.log( message );
         console.log( '-----------------------------------------------------------------' );
